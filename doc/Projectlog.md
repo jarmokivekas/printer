ad60ae60968244615477e7b06cb79f5c5d3bfe66
Juha Kivekas <juha.kivekas@wippies.fi>
2014-02-13 17:04:51 +0200

    Implemented i2C to wav conversion program, added docs

--- doc/Projectlog.md
+++ doc/Projectlog.md
@@ -1,4 +1,18 @@
 Printer
--------
+=======
 
+####11.2.2014
 This project is currently lacking a name. We thought about "Monaco9 is seven pixels tall", but it turns out we want to use a font thats eight pixels tall, so we could go with "We need a font" as the current project name.
+
+####12.2.2014
+There are many thing that i see as intresting and having potential in this project. First of all i like the idea of developing some functional hardware, i know how to program and i know how to solder, but the things in between are a bit fuzzy for me at the moment and i'd like learn some more about that.
+
+I see the result of this project being something autonomous. I hope that it will be a device that can be used without the assistance of a general-purpose computer. This is going to be a printer (kind of) and a printer should be running a program that can take (formatted) input and give output regardless of the source. The input will be given through I2C, the device will save it in RAM, and then it can be printed whenever you want. There should be no need to reprogram the Flash or EEPROM in order to print a new message. The only time reprogramming should be done is if there's an update to the software.
+
+I2C in .WAV
+-----------
+####13.2.2014
+
+I2C is a communicatoin protocol, not a filetype. But it can still be thought of as signals that can be recorded. I2C has two busses, so it can be recorded as stereo audio, one channel for the serial clock and one for the data. WAV is an audiofile format that enables us to save audio in a lossless manner. WAV is also a really simple filetype to write and it's supported by most platforms. These are the reasons why i chose to use WAV as the medium for saving and transporting data to the microcontroller in the electronic brush.
+
+I need four stereo samples (8 bytes of 8-bit samples) to save and transfer a bit of data. This will make sure that the signals are well in sync and that all data is safetly transfered. I'll be using 8kHz 8-bit audio and it'll give me a transfer speed of 2000 bits per second which is good enough for the application. Since the signal has it's own clock the samplerate can be increased to get a faster transfer. A baud of 2000 bits/s should not be hard to recieve with any microcontroller.
---
+++ src/data_to_I2C_audio.c
@@ -0,0 +1,100 @@
+/*
+12.2.2014
+Juha Kivekäs
+
+This is a program that will make data into I2C signals encoded as .wav files.
+The right channel is the serial data and and the left channel is the serial clock.
+The baud of this transfer will be 2000 bits/s.
+*/
+
+
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+uint32_t dataLen;
+uint32_t NumSamples;
+uint32_t SubChunck2Size;
+uint32_t ChunckSize;
+
+
+uint8_t* write_audio(char* filename, uint8_t* header, uint8_t* audio_data){
+    FILE* output = fopen(filename, "w");
+
+    fwrite(header, 0x2c, 1, output);
+    fwrite(audio_data, SubChunck2Size, 1, output);
+    fclose(output);
+}
+
+uint8_t* read_data(char* filename){
+    FILE* input = fopen(filename, "r");
+
+    fseek(input, 0L, SEEK_END);
+    dataLen = ftell(input);
+    fseek(input, 0L, SEEK_SET);
+
+    uint8_t* data = malloc(dataLen);
+    fread(data, dataLen, 1, input);
+    fclose(input);
+    return data;
+}
+int main(int argc, char* argv[]){
+    if ( argc != 3 )
+        printf( "usage: %s input_filename output_filename\n", argv[0]);
+
+    uint8_t* input = read_data(argv[1]);
+
+    //an I2C block per bit (one I2C block has four samples)
+    NumSamples = dataLen * 8 * 4;
+    SubChunck2Size = NumSamples * 2;
+    ChunckSize = SubChunck2Size + 36;
+    //stero sound 
+    uint8_t* audio = malloc(SubChunck2Size);
+
+
+    printf("%d\n", NumSamples);
+    uint8_t WAVE_header[0x2c] = {
+        0x52, 0x49, 0x46, 0x46,     //"RIFF"
+        ((uint8_t*)&ChunckSize)[0], ((uint8_t*)&ChunckSize)[1], ((uint8_t*)&ChunckSize)[2], ((uint8_t*)&ChunckSize)[3],     //36 + SubChunck2size
+        0x57, 0x41, 0x56, 0x45,     //"WAVE"
+
+        0x66, 0x6d, 0x74, 0x20,     //"fmt "
+        0x10, 0x00, 0x00, 0x00,     //16 for PCM
+        0x01, 0x00, 
+        0x02, 0x00, 
+        0x40, 0x1F, 0x00, 0x00,     //8000 samples per second
+        0x80, 0x3e, 0x00, 0x00,     //8000 * 2 * 1
+        0x02, 0x00,                 //2 byte blocks
+        0x08, 0x00,                 //8 bits per sample
+
+        0x64, 0x61, 0x74, 0x61,     //"data"
+        ((uint8_t*)&SubChunck2Size)[0], ((uint8_t*)&SubChunck2Size)[1], ((uint8_t*)&SubChunck2Size)[2], ((uint8_t*)&SubChunck2Size)[3]      //SubChunck2size = NumSamples * 2 * 1
+    };
+
+    uint8_t* currByte = input;
+    uint8_t* currAudio = audio;
+    uint8_t dat;
+    int i, j;
+    for(i=0; i<dataLen; i++){
+        for(j=0; j<8; j++){
+            dat = ((*currByte) & 1) ? 0xff:0x00;
+            currAudio[0] = dat;      //SDA
+            currAudio[1] = 0x00;     //SCL
+            currAudio[2] = dat;
+            currAudio[3] = 0xff;
+            currAudio[4] = dat;
+            currAudio[5] = 0xff;
+            currAudio[6] = dat;
+            currAudio[7] = 0x00;
+            currAudio += 8;
+            *currByte >>= 1;
+        }
+        currByte++;
+    }
+
+    write_audio(argv[2], WAVE_header, audio);
+}
+
+
+
