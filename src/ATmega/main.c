12e9af6774e245e3b2776e41fedfe92213dfe299
Jarmo Kivekas <jarmo.kivekas@guttula.com>
2014-02-18 23:31:26 +0200

    started binding ATmega modules together

---
+++ src/ATmega/main.c
@@ -0,0 +1,13 @@
+#include <avr/io.h>
+#include "main.h"
+#include "twi_slave.h"
+#include "print_ctl.h"
+#include "font.h"
+
+void main(){
+	
+	// Status LEDs are on PORTC pins 0,1,2,3: set as ouputs.
+	DDRC |= (1<<PC0)|(1<<PC1)|(1<<PC2)|(1<<PC3);
+	print_hardware_init();
+	twi_slave_init((1 << 1));
+}
---
+++ src/ATmega/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// Status LED states. Write to PORTC.
+#define LED_RX_OK				// data rx is ongoing, ok
+#define LED_RX_DONE				// data rx is done, ok
+#define LED_RX_BUF_OVF			// rx buffer is full, rx interrupted
+#define LED_RX_ERR				// unknown rx error, rx interrupted
+
+
+#endif
