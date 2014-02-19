b77fb2e0614e6d84e64e43a2e137e85d948542e9
Juha Kivekas <juha.kivekas@wippies.fi>
2014-02-19 22:46:13 +0200

    transmission routines made

---
+++ src/TI-84/i2c_timers.asm
@@ -0,0 +1,60 @@
+next_byte_adress:
+.dw	$0000
+bytes_left:
+.db	$00
+bits_left:
+.db	$00
+phase:
+.db	$00
+
+init_transmission:
+	LD (next_byte_adress), HL
+	LD (bytes_left), D
+	LD A, 0
+	LD (bits_left), A
+	LD A, 1
+	LD (phase), A
+	EI
+	RET
+
+terminate_transmission:
+
+INTERRUPT:
+	DI
+	PUSH AF
+	PUSH HL
+	PUSH BC
+	LD A, (bits_left)
+	JR NZ, phases
+next_byte:
+	LD HL, (next_byte_address)
+	LD A, (HL)
+	LD C, A
+	INC HL
+
+	LD A, (bytes_left)
+	DEC A
+	LD (bytes_left), A
+
+	JR Z, terminate_transmission
+
+	LD A, 8
+next_bit:
+	DEC A
+	LD (bits_left), A
+	LD A, C
+	AND $01		;read one bit
+	OUT (0), A	;set the data
+	CALL wait_transmit
+	XOR $02		;set clock to high
+	OUT (0), A	;send data
+	CALL wait_transmit
+	CALL wait_transmit
+	XOR $02		;set clock to low
+	OUT (0), A
+	CALL wait_transmit
+	SRL C		;next bit
+	DJNZ next_bit
+
+	DEC D
+	JP NZ, next_byte
